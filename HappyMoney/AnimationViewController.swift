df1ec7cee47f0b27a00b2d9c0db3191040d74cf2
ff-yy <yuya12121@gmail.com>
2023-06-12 17:07:34 +0900

    応援ラベルとアニメーション種類の追加、アイコンの変更

--- HappyMoney/AnimationViewController.swift
+++ HappyMoney/AnimationViewController.swift
@@ -14,10 +14,12 @@ class AnimationViewController: UIViewController {
     var subAnimationView = LottieAnimationView()
 
     @IBOutlet var emojiLabel: UILabel!
+    @IBOutlet var cheerLabel: UILabel!
     var element: Element!
     var timer: Timer?
     var elapsedTime: TimeInterval = 0.0
     let happyStringArray: [String] = ["🎉","🥳","😊","🤩","🤑"]
+    let cheerStringArray: [String] = ["いい感じ！","その調子！","素晴らしい！","良いよ！","頑張ってるね！"]
     let happyBGAnimationArray: [String] = ["anime-happybg-confetticannons","anime-happybg-celebration"]
     let moneyMainAnimationArray: [String] = ["anime-moneymain-clapping","anime-moneymain-rupeecoin"]
     let moneyBGAnimationArray: [String] = ["anime-moneybg-rain"]
@@ -37,13 +39,23 @@ class AnimationViewController: UIViewController {
         self.view.addSubview(emojiLabel)
         timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(loopAction), userInfo: nil, repeats: true)
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 10.0) {
-            self.stopLoop()
-        }
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 10.0) {
+//            self.stopLoop()
+//        }
     }
+    
+    func animationLabelOneRotation() {
+        let animation = CAKeyframeAnimation(keyPath: "transform.rotation.z")
+        animation.values = [0, CGFloat.pi * 2]
+        animation.keyTimes = [0, 1]
+        animation.timingFunctions = [CAMediaTimingFunction(name: .easeInEaseOut)]
+        animation.duration = 1.0
+        
+        emojiLabel.layer.add(animation, forKey: "rotationAnimation")
 
-    @objc func loopAction() {
-        // 3秒ごとのループ処理 拡大縮小のアニメーション
+    }
+    
+    func animationLabelBounceSize() {
         UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [.curveEaseInOut], animations: {
             self.emojiLabel.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
         }) { (finished) in
@@ -51,11 +63,31 @@ class AnimationViewController: UIViewController {
                 self.emojiLabel.transform = CGAffineTransform.identity
             }, completion: nil)
         }
+    }
+    
+    func animationLabelBounceY() {
+        let duration: TimeInterval = 1.0
+        let damping: CGFloat = 0.3
+        let initialVelocity: CGFloat = 0.0
         
-        elapsedTime += 3.0
-        if elapsedTime >= 10.0 { // 10秒後には終了
-            stopLoop()
+        UIView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: damping, initialSpringVelocity: initialVelocity, options: [], animations: {
+            self.emojiLabel.transform = CGAffineTransform(translationX: 0, y: -100)
+        }) { (_) in
+            UIView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: damping, initialSpringVelocity: initialVelocity, options: [], animations: {
+                self.emojiLabel.transform = CGAffineTransform.identity
+            }, completion: nil)
         }
+    }
+    
+
+
+    @objc func loopAction() {
+        // 3秒ごとのループ処理 拡大縮小のアニメーション
+//        animationReflection()
+//        elapsedTime += 3.0
+//        if elapsedTime >= 10.0 { // 10秒後には終了
+//            stopLoop()
+//        }
     }
 
     func stopLoop() {
@@ -73,13 +105,15 @@ class AnimationViewController: UIViewController {
         // 絵文字用のラベルの設定
         setEmojiLabel()
         
+        // 応援用のラベルの設定 (ランダム)
+        cheerLabel.text = cheerStringArray[Int.random(in: 0..<cheerStringArray.count)]
+        
         //アニメーションの呼び出し
         addAnimationView()
         
         
         startLoop()
-
-        
+    
 
     }
     
@@ -115,14 +149,21 @@ class AnimationViewController: UIViewController {
         view.sendSubviewToBack(mainAnimationView)
         view.addSubview(subAnimationView)
         view.sendSubviewToBack(subAnimationView)
-
-
     }
 
     
     @IBAction func backToInputView() {
         //viewcontrollerを破棄する
         dismiss(animated: true)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        timer?.invalidate()
+        timer = nil
+        mainAnimationView.stop()
+        subAnimationView.stop()
     }
     
     /*
